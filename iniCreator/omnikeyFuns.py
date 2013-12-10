e086d14526608dbbf864c7dca02e4da99b7948e4
nlannan <nlannan@lexmark.com>
2013-12-10 18:01:40 -0500

    added custom field basic tests.  added output in hex testing, stub for byte swap, and set settings via ucf

--- iniCreator/omnikeyFuns.py
+++ iniCreator/omnikeyFuns.py
@@ -47,15 +47,30 @@ def checkApps():
         print "cardslotsim.fls failed to install on device."
         return False
 
-def runTest():
+def runTest(outSwapped=False, outInHex=False):
+    compare = globals.cardNumber
+    inHexidecimal = 'false'
+    outputSwap = 'false'
+    if outInHex:
+        compare = str(hex(compare).lstrip("0x") or '0').upper()
+        inHexidecimal = 'true'
+    if outSwapped:
+        compare = 'stub'
+        outputSwap = 'true'
+
     loadIni()
+    if outInHex or outSwapped:
+        setSettings(swap=outputSwap, inHex=inHexidecimal)
     deleteEsfLog()
     swipeCard()
     userID = parseEsfLog()
-    if int(userID) == globals.cardNumber:
-        print "cardNumber = \t"+ str(globals.cardNumber)+"\nuserID = \t"+userID+"\n\nPASS"
+
+
+    if userID == str(compare):
+        print "cardNumber = \t"+ str(compare)+"\nuserID = \t"+userID+"\n\nPASS"
     else:
-        print "cardNumber = \t"+ str(globals.cardNumber)+"\nuserID = \t"+userID+"\n\nFAIL"
+        print "cardNumber = \t"+ str(compare)+"\nuserID = \t"+userID+"\n\nFAIL"
+        #sys.exit(1)
 
 
 def loadIni():
@@ -63,6 +78,15 @@ def loadIni():
 
     iniCreator(iniArgs)
 
+    sessionValue = getCurlSession()
+    startCurlSession(sessionValue)
+
+    curlSendCmd = "curl -H \"Cookie: JSESSIONID="+sessionValue+"\" -F settings.inifile=@"+globals.outFileName+" http://" \
+        +globals.ipAddress+"/cgi-bin/direct/printer/prtappauth/admin/configureokservlet"
+    curlSendArgs= shlex.split(curlSendCmd)
+    curlSend, err = subprocess.Popen(curlSendArgs, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+
+def getCurlSession():
     if (globals.printerFamily == 'HS'):
         curlSessionCmd = "curl -D - \"http://"+globals.ipAddress+"/cgi-bin/direct/printer/prtappauth/admin/adminservlet\""
     elif (globals.printerFamily == 'WC'):
@@ -76,21 +100,57 @@ def loadIni():
     cookiePattern = re.compile(r"Set-Cookie: JSESSIONID=([^;]*).*")
     cookieLine = cookiePattern.search(curlSession)
     if cookieLine:
-        sessionValue = cookieLine.group(1)
+        return cookieLine.group(1)
     else:
         print "could not find Cookie for session start"
         sys.exit(1)
+
+def startCurlSession(sessionValue):
     curlStartCmd = "curl \'http://"+globals.ipAddress+"/cgi-bin/direct/printer/prtappauth/admin/appservlet?" \
                    "SelectedAppsNames=omnikey5427ckdriver\' -H \'Referer: http://"+globals.ipAddress+ \
                    "/cgi-bin/direct/printer/prtappauth/admin/\' -H \'Cookie: JSESSIONID="+sessionValue+ \
                    "\' -H \'Connection: keep-alive\'"
     curlStartArgs=shlex.split(curlStartCmd)
     curlStart, err = subprocess.Popen(curlStartArgs, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+    
 
-    curlSendCmd = "curl -H \"Cookie: JSESSIONID="+sessionValue+"\" -F settings.inifile=@"+globals.outFileName+" http://" \
-    +globals.ipAddress+"/cgi-bin/direct/printer/prtappauth/admin/configureokservlet"
+def setSettings(swap='false', inHex='false', beep='false', iClass='254', mifare='254', prox='254', seos='254'):
+    sessionValue = getCurlSession()
+    startCurlSession(sessionValue)
+    
+    curlSessionCmd = "curl http://"+globals.ipAddress+"/cgi-bin/dynamic/printer/config/reports/MenusPage.html"
+    curlSessionArgs= shlex.split(curlSessionCmd)
+    curlSession, err  = subprocess.Popen(curlSessionArgs,stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+    omnikeyPattern = re.compile(r".*Omnikey 5427ck Reader Driver.*=  (.*) <.*")
+    omnikeyLine = omnikeyPattern.search(curlSession)
+    omnikeyVersion = omnikeyLine.group(1)
+
+    writeUCF(omnikeyVersion, swap, inHex, beep, iClass, mifare, prox, seos)
+    sendUCF()
+
+    
+
+def writeUCF(version, dataswap, datahex, beep, iClass, mifare, prox, seos):
+    file = open("omnikey5427.ucf", "w+")
+    file.write("esf.version.omnikey5427ckdriver "+version+"\n")
+    file.write("esf.omnikey5427ckdriver.settings.dataswap \""+dataswap+"\"\n")
+    file.write("esf.omnikey5427ckdriver.settings.datahex \""+datahex+"\"\n")
+    file.write("esf.omnikey5427ckdriver.settings.beep \""+beep+"\"\n")
+    file.write("esf.omnikey5427ckdriver.settings.iClassformat \""+iClass+"\"\n")
+    file.write("esf.omnikey5427ckdriver.settings.miFareformat \""+mifare+"\"\n")
+    file.write("esf.omnikey5427ckdriver.settings.proxformat \""+prox+"\"\n")
+    file.write("esf.omnikey5427ckdriver.settings.seosformat \""+seos+"\"\n")
+#    file.write("esf.omnikey5427ckdriver.settings.inifile \"\"\n")
+#    file.write("esf.omnikey5427ckdriver.settings.inifile.#filename \"\"\n")
+    file.close()
+
+def sendUCF():
+    curlSendCmd = "curl -F \"input_file=@omnikey5427.ucf\" http://"+globals.ipAddress+"/cgi-bin/dynamic/printer/config/secure/importsettings.html"
     curlSendArgs= shlex.split(curlSendCmd)
-    curlSend, err = subprocess.Popen(curlSendArgs, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+    curlSend, err  = subprocess.Popen(curlSendArgs,stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+
+    
+    
 
 def deleteEsfLog():
     curlDebugCmd = "curl \'http://"+globals.ipAddress+"/cgi-bin/direct/printer/prtappse/semenu?loglevel=debug&page=setloglevel\'"
@@ -124,17 +184,43 @@ def parser (searchFile):
     return userID
 
 def successCases():
-    
     #RAW
     globals.cardDataFormat = ['iClassFormat = 0', 'MifareFormat = 0', 'ProxFormat = 0', 'SeosFormat = 0']
     globals.cardNumber = 1012345
+    print "Normal Case:"
     runTest()
+    print "Output in Hex:"
+    runTest(outInHex=True)
+    print "Output swapped:"
+    runTest(outSwapped=True)
+    print "Output in Hex and swapped:"
+    runTest(outInHex=True, outSwapped=True)
+   # print "Max Value:"
+   # globals.cardNumber = 
+   # runTest()
+    print "Min Value:"
+    globals.cardNumber = 0
+    runTest()
+                      
 
     #H10301
     globals.bitLength = 26
     globals.trailingZeros = 6
     globals.cardDataFormat = ['iClassFormat = 1', 'MifareFormat = 1', 'ProxFormat = 1', 'SeosFormat = 1']
     globals.cardNumber = 1012345
+    print "Normal Case:"
+    runTest()
+    print "Output in Hex:"
+    runTest(outInHex=True)
+    print "Output swapped:"
+    runTest(outSwapped=True)
+    print "Output in Hex and swapped:"
+    runTest(outInHex=True, outSwapped=True)
+    print "Max Value:"
+    globals.cardNumber = 255065535
+    runTest()
+    print "Min Value:"
+    globals.cardNumber = 0000000
     runTest()
 
     #H10302
@@ -142,6 +228,19 @@ def successCases():
     globals.trailingZeros = 3
     globals.cardDataFormat = ['iClassFormat = 2', 'MifareFormat = 2', 'ProxFormat = 2', 'SeosFormat = 2']
     globals.cardNumber = 1012345
+    print "Normal Case:"
+    runTest()
+    print "Output in Hex:"
+    runTest(outInHex=True)
+    print "Output swapped:"
+    runTest(outSwapped=True)
+    print "Output in Hex and swapped:"
+    runTest(outInHex=True, outSwapped=True)
+    print "Max Value:"
+    globals.cardNumber = 34359738367
+    runTest()
+    print "Min Value:"
+    globals.cardNumber = 000000000000
     runTest()
 
     #H10304
@@ -149,28 +248,141 @@ def successCases():
     globals.trailingZeros = 3
     globals.cardDataFormat = ['iClassFormat = 4', 'MifareFormat = 4', 'ProxFormat = 4', 'SeosFormat = 4']
     globals.cardNumber = 1012345
+    print "Normal Case:"
     runTest()
-
-    #H10320
+    print "Output in Hex:"
+    runTest(outInHex=True)
+    print "Output swapped:"
+    runTest(outSwapped=True)
+    print "Output in Hex and swapped:"
+    runTest(outInHex=True, outSwapped=True)
+    print "Max Value:"
+    globals.cardNumber = 65535524287 
+    runTest()
+    print "Min Value:"
+    globals.cardNumber = 00000000000
+    runTest()
+   
+    '''
+    #H10320 - Currently causes crash DE28368 
     globals.bitLength = 32
     globals.trailingZeros = 0
     globals.cardDataFormat = ['iClassFormat = 20', 'MifareFormat = 20', 'ProxFormat = 20', 'SeosFormat = 20']
     globals.cardNumber = 1012345
+    print "Normal Case:"
     runTest()
+    sys.exit(0)
+    print "Output in Hex:"
+    runTest(outInHex=True)
+    print "Output swapped:"
+    runTest(outSwapped=True)
+    print "Output in Hex and swapped:"
+    runTest(outInHex=True, outSwapped=True)
+    print "Max Value:"
+    globals.cardNumber = 4294967295  # this is not right find out
+    runTest()
+    print "Min Value:"
+    globals.cardNumber = 00000000
+    runTest()
+    '''
 
     #Corp 1000
     globals.bitLength = 35
     globals.trailingZeros = 5
     globals.cardDataFormat = ['iClassFormat = 100', 'MifareFormat = 100', 'ProxFormat = 100', 'SeosFormat = 100']
-    globals.cardNumber = 101234567
+    globals.cardNumber = 100012345
+    print "Normal Case:"
+    runTest()
+    print "Output in Hex:"
+    runTest(outInHex=True)
+    print "Output swapped:"
+    runTest(outSwapped=True)
+    print "Output in Hex and swapped:"
+    runTest(outInHex=True, outSwapped=True)
+    print "Max Value:"
+    globals.cardNumber = 409501048575
+    runTest()
+    print "Min Value:"
+    globals.cardNumber = 000000000000
     runTest()
 
+    '''
     #Auto
-    globals.cardDataFormat = ['iClassFormat = 254', 'MifareFormat = 254', 'ProxFormat = 254', 'SeosFormat = 254']
-    globals.cardNumber = 1012345
-    runTest()
-
+    #globals.cardDataFormat = ['iClassFormat = 254', 'MifareFormat = 254', 'ProxFormat = 254', 'SeosFormat = 254']
+    #globals.cardNumber = 1012345
+    #print "Normal Case:"
+    #runTest()
+    #print "Output in Hex:"
+    #runTest(outInHex=True)
+    #print "Output swapped:"
+    #runTest(outSwapped=True)
+    #print "Output in Hex and swapped:"
+    #runTest (outInHex=True, outSwapped=True)
+    '''
+    '''
+    #Currently causes crash - DE28378
     #Customer Defined
+    globals.cardDataFormat = ['iClassFormat = 255', 'MifareFormat = 255', 'ProxFormat = 255', 'SeosFormat = 255']
+    globals.cardNumber = 1012345
+    globals.customFields = [(0,20),(0,0),(0,0),(0,0)]
+    globals.trailingZeros = 4
+    globals.bitLength = 20
+    print "Normal Case, 1 field:"
+    print "A field"
+    runTest()
+    globals.customFields = [(0,0),(0,20),(0,0),(0,0)]
+    print "B field"
+    runTest()
+    globals.customFields = [(0,0),(0,0),(0,20),(0,0)]
+    print "C field"
+    runTest()
+    globals.customFields = [(0,0),(0,0),(0,0),(0,20)]
+    print "D field"
+    runTest()
+    print "Output in Hex:"
+    runTest(outInHex=True)
+    print "Output swapped:"
+    runTest(outSwapped=True)
+    print "Output in Hex and swapped:"
+    runTest (outInHex=True, outSwapped=True)
+
+    globals.customFields = [(16,8),(0,16),(0,0),(0,0)]
+    globals.trailingZeros = 0
+    globals.bitLength = 24
+    print "Normal Case, 2 fields:"
+    runTest()
+    print "Output in Hex:"
+    runTest(outInHex=True)
+    print "Output swapped:"
+    runTest(outSwapped=True)
+    print "Output in Hex and swapped:"
+    runTest (outInHex=True, outSwapped=True)
+
+    globals.customFields = [(12,7),(6,6),(0,6),(0,0)]
+    globals.trailingZeros = 5
+    globals.bitLength = 19
+    print "Normal Case, 3 fields:"
+    runTest()
+    print "Output in Hex:"
+    runTest(outInHex=True)
+    print "Output swapped:"
+    runTest(outSwapped=True)
+    print "Output in Hex and swapped:"
+    runTest (outInHex=True, outSwapped=True)
+
+    globals.customFields = [(18,6),(12,6),(6,6),(0,6)]
+    globals.trailingZeros = 0
+    globals.bitLength = 24
+    print "Normal Case, 4 fields:"
+    runTest()
+    print "Output in Hex:"
+    runTest(outInHex=True)
+    print "Output swapped:"
+    runTest(outSwapped=True)
+    print "Output in Hex and swapped:"
+    runTest (outInHex=True, outSwapped=True)
+    '''
+
 
     
     
