5c8fa31516314879cf7faa0eb246851da4e9d0bd
nlannan <natelannan@gmail.com>
2013-12-16 00:15:50 -0500

    added pacs extractor to driver

--- iniCreator/driver.py
+++ iniCreator/driver.py
@@ -2,22 +2,31 @@
 
 
 from iniCreator import iniCreator
+import sys, re
 
 
-cardNumber = 1012345
-bitLength = 24
-cardDataFormat = ['iClassFormat = 42', 'MifareFormat = 0', 'ProxFormat = 255', 'SeosFormat = 100']
+cardNumber = '1012345'
+bitLength = 36
+cardDataFormat = ['iClassFormat = 42', 'MifareFormat = 0', 'ProxFormat = 254', 'SeosFormat = 100']
 activeCard = 'Prox'
-trailingZeros = 0
+trailingZeros = 4
 outFileName = 'foo.ini'
-customFields = [(16,8),(0,16),(0,0),(0,0)]
+customFields = [(21,12),(1,20),(0,0),(0,0)]
 
 args = [cardNumber, bitLength, cardDataFormat, activeCard, trailingZeros, outFileName, customFields]
 #args = []
 iniCreator(args)
+pacsPattern = re.compile(r"PACSBits = (.*)")
+buff = open(outFileName)
+pacsLine = pacsPattern.search(buff.read())
+if pacsLine:
+  pacs =  pacsLine.group(1)
+else:
+  print "could not find PACS"
+  sys.exit(1)
 
-#print customFields[1][0]
-#print customFields[1][1]
+pacs = pacs[2:]
+print pacs
 
 
 
